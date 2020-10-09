92b56f037f2aa8865e4c08e925bfcb098a29bb3f
Eric Huang <ericwhhuang@gmail.com>
2020-10-09 09:51:18 -0700

    Add setting wizard activity screen

---
+++ app/src/main/java/ca/bcit/beproductiv/SettingsWizardActivity.java
@@ -0,0 +1,14 @@
+package ca.bcit.beproductiv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingsWizardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_wizard);
+    }
+}
