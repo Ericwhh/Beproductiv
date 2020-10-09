24fc5a79c866f654eec8f948c157f3299be4a34e
Eric Huang <ericwhhuang@gmail.com>
2020-10-09 10:03:31 -0700

    Link main screen with settings screen

--- app/src/main/java/ca/bcit/beproductiv/MainActivity.java
+++ app/src/main/java/ca/bcit/beproductiv/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         settingsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SettingsWizardActivity.class);
+                Intent intent = new Intent(MainActivity.this, Settings.class);
                 startActivity(intent);
             }
         });
