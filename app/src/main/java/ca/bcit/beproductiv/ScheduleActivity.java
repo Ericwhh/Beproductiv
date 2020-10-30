a3757668485b7e3d031fb2a4e1681c13407755f5
Eric Huang <ericwhhuang@gmail.com>
2020-10-30 00:58:41 -0700

    Add button in scheduler activity to add a new timer using a new activity

--- app/src/main/java/ca/bcit/beproductiv/ScheduleActivity.java
+++ app/src/main/java/ca/bcit/beproductiv/ScheduleActivity.java
@@ -1,14 +1,38 @@
 package ca.bcit.beproductiv;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CalendarView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ScheduleActivity extends AppCompatActivity {
+    CalendarView calendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
+
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                // TODO: Modifies the fragment
+            }
+        });
+
+        FloatingActionButton addToScheduleButton = findViewById(R.id.addTimeToSchedule);
+        addToScheduleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScheduleActivity.this, AddTimeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
