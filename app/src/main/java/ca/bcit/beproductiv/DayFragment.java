b4bf8e56390f9e589b972b4f12a2f1917d074220
Eric Huang <ericwhhuang@gmail.com>
2020-11-06 08:50:28 -0800

    Remove extra code

--- app/src/main/java/ca/bcit/beproductiv/DayFragment.java
+++ app/src/main/java/ca/bcit/beproductiv/DayFragment.java
@@ -14,16 +14,6 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class DayFragment extends Fragment {
-/*
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-*/
     public DayFragment() {
         // Required empty public constructor
     }
@@ -31,40 +21,7 @@ public class DayFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
-        View v = getView();
-        if (v != null) {
-
-        }
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DayFragment.
-     */
-    /*
-    // TODO: Rename and change types and number of parameters
-    public static DayFragment newInstance(String param1, String param2) {
-        DayFragment fragment = new DayFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-*/
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
