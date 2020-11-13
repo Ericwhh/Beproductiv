3bd5769c09bc403970b1f898761cb0dd00a0c34b
Eric Huang <ericwhhuang@gmail.com>
2020-11-13 14:18:20 -0800

    Add settings tab using PreferencesScreen and style preferences with xml layouts

---
+++ app/src/main/java/ca/bcit/beproductiv/Tabs/SettingsFragment.java
@@ -0,0 +1,48 @@
+package ca.bcit.beproductiv.Tabs;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import ca.bcit.beproductiv.R;
+
+public class SettingsFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        getParentFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings, new PreferencesFragment())
+                .commit();
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+    public static class PreferencesFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.preferences, rootKey);
+        }
+
+        @Override
+        public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+
+            final RecyclerView recyclerView = getListView();
+            recyclerView.setPadding(0, 0, 0, 48);
+        }
+    }
+}
