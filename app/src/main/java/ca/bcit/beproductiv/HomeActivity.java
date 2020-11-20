d512e136f33ea762c16be89a97ce4a5249eff078
NoahMacRitchie <47670496+NoahMacRitchie@users.noreply.github.com>
2020-11-20 08:57:59 -0800

    Removed accidental changes to Home Activity

--- app/src/main/java/ca/bcit/beproductiv/HomeActivity.java
+++ app/src/main/java/ca/bcit/beproductiv/HomeActivity.java
@@ -4,14 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.preference.ListPreference;
-import androidx.preference.PreferenceManager;
-import androidx.room.Room;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
@@ -44,9 +40,6 @@ public class HomeActivity extends AppCompatActivity {
         ViewPager pager = findViewById(R.id.pager);
         pager.setAdapter(pagerAdapter);
 
-        SharedPreferences sharedConfig = PreferenceManager.getDefaultSharedPreferences(this);
-
-        System.out.println(sharedConfig.getString("interval_focus", "default"));
 
 //        Button settingsButton = findViewById(R.id.settingsButton);
 //        settingsButton.setOnClickListener(new View.OnClickListener() {
