001ec6784996f19147771a6db5112e9b08d31437
agroyelov <andrey.goryelov@gmail.com>
2020-11-29 16:45:18 -0800

    Fixed editing completed to-do

--- app/src/main/java/ca/bcit/beproductiv/TodoItemForm.java
+++ app/src/main/java/ca/bcit/beproductiv/TodoItemForm.java
@@ -31,6 +31,7 @@ public class TodoItemForm extends AppCompatActivity {
     int todoUID = DEFAULT_TODO_UID;
     String todoName = "";
     String todoDescription = "";
+    boolean todoComplete = false;
 
     Button btnSaveTodo;
     Button btnDeleteTodo;
@@ -65,9 +66,11 @@ public class TodoItemForm extends AppCompatActivity {
         if (FORM_ACTION == FormAction.Edit) {
             tvFormHeading.setText("Edit Todo");
 
-            todoUID = getIntent().getIntExtra("TODO_UID", DEFAULT_TODO_UID);
-            todoName = getIntent().getStringExtra("TODO_NAME");
-            todoDescription = getIntent().getStringExtra("TODO_DESCRIPTION");
+            Intent i = getIntent();
+            todoUID = i.getIntExtra("TODO_UID", DEFAULT_TODO_UID);
+            todoName = i.getStringExtra("TODO_NAME");
+            todoDescription = i.getStringExtra("TODO_DESCRIPTION");
+            todoComplete = i.getBooleanExtra("TODO_COMPLETE", false);
         } else {
             tvFormHeading.setText("Add Todo");
 
@@ -85,7 +88,7 @@ public class TodoItemForm extends AppCompatActivity {
 
                 if (todoName.trim().isEmpty() || todoDescription.trim().isEmpty()) return;
 
-                TodoItem item = new TodoItem(todoName, todoDescription);
+                TodoItem item = new TodoItem(todoName, todoDescription, todoComplete);
 
                 if (FORM_ACTION == FormAction.Add) {
                     new AddTodoItemsAsync(getApplicationContext()).execute(item);
